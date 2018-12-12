16164bb38bc1b556f383a39a6b814618a0bf83d0
summerJY100 <15210365768@163.com>
2018-12-12 16:36:08 +0800

    新增Pojo类

---
+++ src/main/java/com/nw/model/H199.java
@@ -0,0 +1,45 @@
+package com.nw.model;
+
+import java.util.Date;
+
+public class H199 {
+    private Long id;
+
+    private Date vTime;
+
+    private Float v;
+
+    public H199(Long id, Date vTime, Float v) {
+        this.id = id;
+        this.vTime = vTime;
+        this.v = v;
+    }
+
+    public H199() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getvTime() {
+        return vTime;
+    }
+
+    public void setvTime(Date vTime) {
+        this.vTime = vTime;
+    }
+
+    public Float getV() {
+        return v;
+    }
+
+    public void setV(Float v) {
+        this.v = v;
+    }
+}
